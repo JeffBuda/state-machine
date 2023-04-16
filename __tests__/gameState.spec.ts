815b55a859bf97d8459e5852e789aca0bcc40136
Jeff Buda <budajeff@gmail.com>
2023-04-16 19:08:06 -0400

    added assertion

--- __tests__/gameState.spec.ts
+++ __tests__/gameState.spec.ts
@@ -3,11 +3,11 @@ import { GameState, GameStateAction, executeGameAction } from '../src/gameState'
 
 describe('GameStateAction.Initialize', () => {
     it('should property configure all dominos', () => {
-        const gameState = executeGameAction(GameStateAction.Initialize, { player1: '1', player2: '2' });
-        expect(dominosOfState(gameState.dominos, DominoState.OutOfGame).length).toBe(24);
-        expect(dominosOfState(gameState.dominos, DominoState.InDrawPile).length).toBe(24 - 4);
-        expect(dominosOfState(gameState.dominos, DominoState.InPickList_Available).length).toBe(4);
-        expect(gameState.state).toBe(GameState.Pick1_Player1);
+        const gameData = executeGameAction(GameStateAction.Initialize, { player1: '1', player2: '2' });
+        expect(dominosOfState(gameData.dominos, DominoState.OutOfGame).length).toBe(24);
+        expect(dominosOfState(gameData.dominos, DominoState.InDrawPile).length).toBe(24 - 4);
+        expect(dominosOfState(gameData.dominos, DominoState.InPickList_Available).length).toBe(4);
+        expect(gameData.state).toBe(GameState.Pick1_Player1);
     })
 });
 
@@ -15,7 +15,7 @@ describe('GameStateAction.Pick1_Player2', () => {
     it('should property configure all dominos', () => {
 
         let gameData = executeGameAction(GameStateAction.Initialize, { player1: '1', player2: '2' });
-        
+
         gameData = executeGameAction(GameStateAction.Pick_Player1,
             {
                 //pick the first domino in the pick list
@@ -23,6 +23,9 @@ describe('GameStateAction.Pick1_Player2', () => {
             },
             gameData);
 
-            expect(gameData.state).toBe(GameState.Pick1_Player2);
-        })
+        expect(gameData.state).toBe(GameState.Pick1_Player2);
+        expect(dominosOfState(gameData.dominos, DominoState.InPickList_Claimed).length).toBe(1);
+        expect(dominosOfState(gameData.dominos, DominoState.InPickList_Available).length).toBe(3);
+
+    })
 });
