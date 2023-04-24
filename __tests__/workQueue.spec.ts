2f720e4bf092fe8d5e2ef0aeb8d448e074cd74f9
Jeff Buda <budajeff@gmail.com>
2023-04-23 20:23:05 -0400

    added sequence test

--- __tests__/workQueue.spec.ts
+++ __tests__/workQueue.spec.ts
@@ -1,24 +1,43 @@
 import { DominoState, getDominosByState } from '../src/domino';
-import {PlayKind, generateState, processCommand} from '../src/workQueue';
+import { PlayKind, generateState, processCommand } from '../src/workQueue';
 
-describe('work queue', ()=>{
-    it('has some items', ()=>{
+describe('work queue', () => {
+    it('has some items', () => {
         var state = generateState(['jeff', 'dave']);
-        expect(state.steps.length).toBeGreaterThan(1); 
+        expect(state.steps.length).toBeGreaterThan(1);
     });
-    describe('executeCommand', ()=>{
-        it('rejects invalid commands', ()=>{
+    describe('executeCommand', () => {
+        it('rejects invalid commands', () => {
             var state = generateState(['jeff', 'dave']);
-            const result = processCommand(state, {playerName: 'dave', playKind: PlayKind.ClaimDomino, dominoId: 0});
+            const result = processCommand(state, { playerName: 'dave', playKind: PlayKind.ClaimDomino, dominoId: 0 });
             expect(result.success).toBeFalsy();
         });
-        it('accepts valid commands', ()=>{
+        it('accepts valid commands', () => {
             var state = generateState(['jeff', 'dave']);
             const domino = getDominosByState(state.dominos, DominoState.InPickList_Available);
-            const result = processCommand(state, {playerName: 'jeff', playKind: PlayKind.ClaimDomino, dominoId: domino[0].rank});
+            const result = processCommand(state, { playerName: 'jeff', playKind: PlayKind.ClaimDomino, dominoId: domino[0].rank });
             expect(result.success).toBeTruthy();
             expect(getDominosByState(result.state.dominos, DominoState.InPickList_Claimed).length).toBe(1);
+        });
+        it('can calculate the correct player sequence for the first round', () => {
+            var state = generateState(['jeff', 'dave']);
 
+            var domino = getDominosByState(state.dominos, DominoState.InPickList_Available);
+            var result = processCommand(state, { playerName: 'jeff', playKind: PlayKind.ClaimDomino, dominoId: domino[0].rank });
+            
+            domino = getDominosByState(state.dominos, DominoState.InPickList_Available);
+            result = processCommand(state, { playerName: 'dave', playKind: PlayKind.ClaimDomino, dominoId: domino[0].rank });
+            
+            domino = getDominosByState(state.dominos, DominoState.InPickList_Available);
+            result = processCommand(state, { playerName: 'jeff', playKind: PlayKind.ClaimDomino, dominoId: domino[0].rank });
+            
+            domino = getDominosByState(state.dominos, DominoState.InPickList_Available);
+            result = processCommand(state, { playerName: 'dave', playKind: PlayKind.ClaimDomino, dominoId: domino[0].rank });
+
+            expect(getDominosByState(result.state.dominos, DominoState.InPickList_Claimed).length).toBe(4);
+            expect(getDominosByState(result.state.dominos, DominoState.InPickList_Available).length).toBe(0);
+
+            // place the first domino
         });
     });
 });
--- src/workQueue.ts
+++ src/workQueue.ts
@@ -11,6 +11,7 @@ export enum PlayKind {
 export interface IStep {
     playerName?: string;
     readonly playKind: PlayKind;
+    dominoId?: number;
 }
 
 export interface ICommand {
@@ -35,22 +36,22 @@ export function generateState(playerNames: string[]): IState {
 
     if (playerNames.length !== 2) throw new Error();
 
-    const steps = [
-        { playerName: playerNames[0], playKind: PlayKind.ClaimDomino },
-        { playerName: playerNames[1], playKind: PlayKind.ClaimDomino },
-        { playerName: playerNames[0], playKind: PlayKind.ClaimDomino },
-        { playerName: playerNames[1], playKind: PlayKind.ClaimDomino },
-        { playerName: undefined, playKind: PlayKind.CalculateNextRound }
+    const steps: IStep[] = [
+        { playerName: playerNames[0], playKind: PlayKind.ClaimDomino, dominoId: undefined },
+        { playerName: playerNames[1], playKind: PlayKind.ClaimDomino, dominoId: undefined },
+        { playerName: playerNames[0], playKind: PlayKind.ClaimDomino, dominoId: undefined },
+        { playerName: playerNames[1], playKind: PlayKind.ClaimDomino, dominoId: undefined },
+        { playerName: undefined, playKind: PlayKind.CalculateNextRound, dominoId: undefined }
     ];
 
     for (var i = 0; i < 10; i++) {
         const roundSteps = [
-                { playerName: undefined, playKind: PlayKind.PlaceDomino },
-                { playerName: undefined, playKind: PlayKind.ClaimDomino },
-                { playerName: undefined, playKind: PlayKind.PlaceDomino },
-                { playerName: undefined, playKind: PlayKind.ClaimDomino },
-                { playerName: undefined, playKind: PlayKind.CalculateNextRound }
-            ];
+            { playerName: undefined, playKind: PlayKind.PlaceDomino },
+            { playerName: undefined, playKind: PlayKind.ClaimDomino },
+            { playerName: undefined, playKind: PlayKind.PlaceDomino },
+            { playerName: undefined, playKind: PlayKind.ClaimDomino },
+            { playerName: undefined, playKind: PlayKind.CalculateNextRound }
+        ];
         steps.push(...roundSteps);
     }
 
@@ -76,39 +77,52 @@ export function processCommand(state: IState, command: ICommand): ICommandResult
         updatedState.steps.shift();
 
         // mark domino as claimed
-        if(!getDominosByState(state.dominos, DominoState.InPickList_Available).find(d => d.rank === command.dominoId)) {
-            return {command, state, success: false, message: 'Domino not available'};
+        if (!getDominosByState(state.dominos, DominoState.InPickList_Available).find(d => d.rank === command.dominoId)) {
+            return { command, state, success: false, message: 'Domino not available' };
         }
         updatedState.dominos = calcDominoClaimed(updatedState.dominos, command.playerName, command.dominoId);
 
-        if (updatedState.steps[0].playKind === PlayKind.CalculateNextRound) {
-            // mark calc step as complete
-            updatedState.steps.shift();
-
-            // set player order for next round
-            const order = calcPlayerTurnOrder(updatedState.dominos);
-            for (let i = 0; i < 4; i++) {
-                updatedState.steps[i].playerName = order[i];
-            }
-        }
+        calcPlayerOrderForNextRound(updatedState);
 
     } else if (currentStep.playKind === PlayKind.PlaceDomino) {
+
+        if (command.playerName !== currentStep.playerName ||
+            command.playKind !== currentStep.playKind ||
+            command.dominoId !== currentStep.dominoId) {
+            return { command, state, success: false, message: 'Invalid Player, Play, or Domino.' };
+        }
 
         // mark step as complete
         updatedState.steps.shift();
 
         // mark domino as placed
         updatedState.dominos = calcDominoPlaced(updatedState.dominos, command.playerName, command.dominoId);
+
+        calcPlayerOrderForNextRound(updatedState);
     }
 
     return { success: true, command: command, state: updatedState, message: 'Success' };
 }
 
-function calcPlayerTurnOrder(dominos: Domino[]): string[] {
+function calcPlayerOrderForNextRound(updatedState: { dominos: Domino[]; steps: IStep[]; }) {
+    if (updatedState.steps[0].playKind === PlayKind.CalculateNextRound) {
+        // mark calc step as complete
+        updatedState.steps.shift();
+
+        // set player order for next round
+        const claimedDominos = getSortedClaimedDominos(updatedState.dominos);
+        for (let i = 0; i < 4; i++) {
+            updatedState.steps[i].playerName = claimedDominos[i].pickedBy;
+            updatedState.steps[i].dominoId = claimedDominos[i].rank;
+        }
+    }
+}
+
+function getSortedClaimedDominos(dominos: Domino[]): Domino[] {
     const claimed = getDominosByState(dominos, DominoState.InPickList_Claimed);
     claimed.sort((a, b) => {
         if (a.rank === b.rank) return 0;
-        return a.rank > b.rank ? -1 : 1;
+        return a.rank > b.rank ? 1 : -1;
     });
-    return claimed.map(d => d.pickedBy!);
+    return claimed;
 }
